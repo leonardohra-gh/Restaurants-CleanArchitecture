96dcff4444fe335b26529a3bb8357b8d4654a194
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2025-03-14 20:45:02 -0300

    Criando as classes do domínio (aula 19 fim)

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Entities
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
