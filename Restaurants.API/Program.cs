1fe867af41df70c9c88af2296d3b3d981748d171
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2025-03-17 10:20:06 -0300

    Refatorando serilog (aula 42 fim)

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -13,10 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Host.UseSerilog((context, configuration) =>
-    configuration
-        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine} {Message:lj}{NewLine}{Exception}")
+    configuration.ReadFrom.Configuration(context.Configuration)
 );
 
 var app = builder.Build();
