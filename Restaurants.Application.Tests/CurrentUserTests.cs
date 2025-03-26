f4c78196b86facfc997240f90f4d9bf26efadc55
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2025-03-26 14:30:24 -0300

    Criando testes para user context (aula 83 fim)

--- Restaurants.Application.Tests/CurrentUserTests.cs
+++ Restaurants.Application.Tests/CurrentUserTests.cs
@@ -1,10 +1,51 @@
-﻿namespace Restaurants.Application.Tests;
+﻿using FluentAssertions;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Constants;
+
+namespace Restaurants.Application.Tests;
 
 public class CurrentUsersTests
 {
-    [Fact]
-    public void IsInRoleTest()
+    // TestMethod_Scenario_ExpectedResult
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void IsInRoleTest_WithMatchingRole_ShouldReturnTrue(string roleName)
     {
+        //Arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
 
+        // Act
+        var isInRole = currentUser.IsInRole(roleName);
+
+        // Assert
+        isInRole.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void IsInRoleTest_WithNoMatchingRole_ShouldReturnFalse()
+    {
+        //Arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // Act
+        var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+        // Assert
+        isInRole.Should().BeFalse();
+    }
+
+
+    [Fact()]
+    public void IsInRoleTest_WithMatchingRoleCase_ShouldReturnFalse()
+    {
+        //Arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // Act
+        var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+
+        // Assert
+        isInRole.Should().BeFalse();
     }
 }
